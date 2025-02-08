5895ad5ce5abdedeb9b8ee5379e25b651a600f85
sakura-yuzu <katakuripowder@gmail.com>
2025-02-08 15:18:03 +0900

    feat: リスト表示 #1

---
+++ Assets/PFEnemies/Macros/CreatureSetting.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+[SerializeField]
+public class CreatureSetting : ScriptableObject
+{
+  public string displayName;
+  public int hp;
+  public int mp;
+  public int attackPower;
+  public int defensePower;
+  public int speed;
+  public string prefabAddress;
+  public string description;
+}
--- Assets/PFEnemies/Macros/SceneController.cs
+++ Assets/PFEnemies/Macros/SceneController.cs
@@ -15,13 +15,33 @@ class SceneController : MonoBehaviour
 	public Button SummonButton;
 	public Button CriticalAttackButton;
 	public Button DodgeButton;
+	public CreatureList CreatureList;
+	public GameObject CharacterList;
+	public GameObject cube;
+	public Transform PreviewCube;
+	public Button SelectButton;
+	private Dictionary<string, GameObject> creatureInstances = new Dictionary<string, GameObject>();
+	private int UIViewLayer;
 	void Awake()
 	{
 		// Set the target frame rate to 60fps
 		Application.targetFrameRate = 60;
+		Prepare();
 	}
 
 	void Start()
 	{
+	}
+
+	private async void Prepare()
+	{
+		UIViewLayer = LayerMask.NameToLayer("UIView");
+		var ToggleGroup = CharacterList.GetComponent<ToggleGroup>();
+		var buttonPrefab = await Addressables.LoadAssetAsync<GameObject>("Assets/PFEnemies/Prefabs/Toggle.prefab").Task;
+		foreach (CreatureSetting creature in CreatureList.creatures)
+		{
+			GameObject toggle = Instantiate(buttonPrefab, CharacterList.transform, false);
+			toggle.transform.Find("Label").GetComponent<Text>().text = creature.displayName;
+			toggle.GetComponent<Toggle>().group = ToggleGroup;
 	}
 }
